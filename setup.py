aa07395bd948530ce92572efb5ccb7b9987c6298
Madison Scott-Clary <madison.scott-clary@canonical.com>
2016-11-08 16:14:28 -0700

    Initial package

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+
+setup(
+    name='prose-wc',
+    version='0.1',
+    description='Jekyll-aware prose wordcount utility',
+    author='Madison Scott-Clary',
+    author_email='makyo@drab-makyo.com',
+    install_requires=['argparse', 'pyyaml'],
+    packages=find_packages(),
+    setup_requires=['nose>=1.0'],
+    tests_require=['nose', 'coverage', 'mock'],
+    entry_points={
+        'console_scripts': [
+            'prose-wc = prose_wc.wc:run',
+        ],
+    },
+    url='https://github.com/makyo/prose-wc',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+    ])
